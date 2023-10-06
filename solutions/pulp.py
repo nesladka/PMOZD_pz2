c9dcb36dc0d3654d28e761dc038a9a954051631b
nesladka <147166345+nesladka@users.noreply.github.com>
2023-10-06 22:03:26 +0300

    Update pulp.py

--- solutions/pulp.py
+++ solutions/pulp.py
@@ -1,1 +1,27 @@
+import pulp
+import time
 
+start = time.time()
+
+x1 = pulp.LpVariable("x1", lowBound=0)
+x2 = pulp.LpVariable("x2", lowBound=0)
+
+problem = pulp.LpProblem('0', pulp.LpMaximize)
+problem += 2 * x1 - x2, "Функция цели"
+problem += x1 <= 3, "1"
+problem += x1 >= -1, "2"
+problem += -2 * x1 - 3 * x2 <= 6, "3"
+problem += -x1 + 2 * x2 <= 6, "4"
+
+problem.solve()
+
+print("Результат:")
+for variable in problem.variables():
+    print(variable.name, "=", variable.varValue)
+
+print("Прибыль:")
+print(pulp.value(problem.objective))
+
+stop = time.time()
+print("Время :")
+print(stop - start)
